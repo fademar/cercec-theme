42e420bee07f95417d72dd41b1d28f0a2a77e974
fademar <fdemarthon@gmail.com>
2018-04-26 14:34:56 +0200

    header menu

--- footer.php
+++ footer.php
@@ -11,26 +11,25 @@
 
 ?>
 
-	</div><!-- #content -->
+			</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cercec-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cercec-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cercec-theme' ), 'cercec-theme', '<a href="https://www.cercec.fr">Fabrice Demarthon</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+			<footer id="colophon" class="site-footer">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cercec-theme' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'cercec-theme' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep"> | </span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'cercec-theme' ), 'cercec-theme', '<a href="https://www.cercec.fr">Fabrice Demarthon</a>' );
+						?>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
 
-<?php wp_footer(); ?>
-
+		<?php wp_footer(); ?> 
 </body>
 </html>
--- header.php
+++ header.php
@@ -21,34 +21,37 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cercec-theme' ); ?></a>
+		<div id="page" class="site">
+			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cercec-theme' ); ?></a>
 
-		<header id="masthead" class="container site-header">
-			<div class="row justify-content-center">
-				<?php
-
-				if ( has_custom_logo() ) : 
-					the_custom_logo();
-					else :?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-
-
+			<header id="masthead" class="container site-header">
+				<div class="row justify-content-center">
 					<?php
-					endif;?>
-				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cercec-theme' ); ?></button>
-					<div class="col align-self-center">
+					if ( has_custom_logo() ) : ?> 
+						<div class="custom-logo">
+							<?php the_custom_logo(); ?>
+						</div>
+						<?php 
+						else :?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+
 						<?php
-						wp_nav_menu( array(
-							'theme_location' => 'menu-1',
-							'menu_id'        => 'primary-menu',
-						) );
-						?>
-					</div>
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
+						endif;?>
+					</div><!-- .site-branding -->
 
-			<div id="content" class="site-content">
+					<nav id="site-navigation" class="main-navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cercec-theme' ); ?></button>
+						<div class="col align-self-center">
+							<?php
+							wp_nav_menu( array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu',
+							) );
+							?>
+						</div>
+					</nav><!-- #site-navigation -->
+				</header><!-- #masthead -->
+
+				<div id="content" class="site-content">
